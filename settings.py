343de3b266bf323fc60b617ed6dad7480293863a
0nemed0 <87437931+0nemed0@users.noreply.github.com>
2024-12-10 12:23:52 +0800

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,27 @@
+# ORIGINAL file was from Rampal Punia
+
+from pathlib import Path
+import sys
+
+# Get the absolute path of the current file
+FILE = Path(__file__).resolve()
+# Get the parent directory of the current file
+ROOT = FILE.parent
+# Add the root path to the sys.path list if it is not already there
+if ROOT not in sys.path:
+    sys.path.append(str(ROOT))
+# Get the relative path of the root directory with respect to the current working directory
+ROOT = ROOT.relative_to(Path.cwd())
+
+
+# Videos config
+VIDEO_DIR = ROOT / 'videos'
+VIDEOS_DICT = {
+    'video_1': VIDEO_DIR / 'video_1.mp4',
+    #'video_2': VIDEO_DIR / 'video_2.mp4',
+    #'video_3': VIDEO_DIR / 'video_3.mp4',
+}
+
+
+# Webcam
+WEBCAM_PATH = 0
